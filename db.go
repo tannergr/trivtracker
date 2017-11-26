ae054db571079634cbd587c0146c6b867783e289
tannergr <tanner@tlgraham.com>
2017-11-26 13:13:10 -0800

    moved creds to env variables

--- db.go
+++ db.go
@@ -6,6 +6,15 @@ import (
   _ "github.com/lib/pq"
   "database/sql"
   "fmt"
+  "os"
+)
+
+const (
+        DB_USER = os.Getenv("DB_USER")
+        DB_PASSWORD = os.Getenv("DB_PASSWORD")
+        DB_NAME = os.Getenv("DB_NAME")
+        DB_HOST = os.Getenv("DB_HOST")
+        DB_PORT = os.Getenv("DB_PORT")
 )
 
 func initDB(){
